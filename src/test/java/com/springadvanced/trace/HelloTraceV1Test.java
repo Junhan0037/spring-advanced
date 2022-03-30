597b533ae09cbadaaadd7ff9215660d95aa55f00
Junhan0037 <wnsgks0037@naver.com>
2022-03-30 22:05:45 +0900

    feat: 로그 추적기 V1 - 프로토타입 개발 (2)

---
+++ src/test/java/com/springadvanced/trace/HelloTraceV1Test.java
@@ -0,0 +1,22 @@
+package com.springadvanced.trace;
+
+import org.junit.jupiter.api.Test;
+
+class HelloTraceV1Test {
+
+
+    @Test
+    void begin_end() {
+        HelloTraceV1 trace = new HelloTraceV1();
+        TraceStatus status = trace.begin("hello");
+        trace.end(status);
+    }
+
+    @Test
+    void begin_exception() {
+        HelloTraceV1 trace = new HelloTraceV1();
+        TraceStatus status = trace.begin("hello");
+        trace.exception(status, new IllegalStateException());
+    }
+
+}
