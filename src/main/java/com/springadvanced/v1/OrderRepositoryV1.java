f96cf729f5a53e66f18a618360bb5e7718d62d0b
Junhan0037 <wnsgks0037@naver.com>
2022-03-31 23:43:26 +0900

    feat: 로그 추적기 V1 - 적용

---
+++ src/main/java/com/springadvanced/v1/OrderRepositoryV1.java
@@ -0,0 +1,40 @@
+package com.springadvanced.v1;
+
+import com.springadvanced.trace.HelloTraceV1;
+import com.springadvanced.trace.TraceStatus;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV1 {
+
+    private final HelloTraceV1 trace;
+
+    public void save(String itemId) {
+        TraceStatus status = null;
+        try {
+            status = trace.begin("OrderRepository.request()");
+
+            // 저장 로직
+            if (itemId.equals("ex")) {
+                throw new IllegalStateException("예외 발생!");
+            }
+            sleep(1000);
+
+            trace.end(status);
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ src/main/java/com/springadvanced/v1/OrderServiceV1.java
@@ -0,0 +1,27 @@
+package com.springadvanced.v1;
+
+import com.springadvanced.trace.HelloTraceV1;
+import com.springadvanced.trace.TraceStatus;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceV1 {
+
+    private final OrderRepositoryV1 orderRepository;
+    private final HelloTraceV1 trace;
+
+    public void orderItem(String itemId) {
+        TraceStatus status = null;
+        try {
+            status = trace.begin("OrderService.request()");
+            orderRepository.save(itemId);
+            trace.end(status);
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
+    }
+
+}
