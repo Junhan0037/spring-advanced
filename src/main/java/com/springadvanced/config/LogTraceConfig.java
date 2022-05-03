977d0d47aa19cbc56e4f51fe9bf49ff2a1c81dab
Junhan0037 <wnsgks0037@naver.com>
2022-05-03 21:21:29 +0900

    feat: 쓰레드 로컬 동기화 - 적용

--- src/main/java/com/springadvanced/config/LogTraceConfig.java
+++ src/main/java/com/springadvanced/config/LogTraceConfig.java
@@ -1,7 +1,7 @@
 package com.springadvanced.config;
 
-import com.springadvanced.trace.logtrace.FieldLogTrace;
 import com.springadvanced.trace.logtrace.LogTrace;
+import com.springadvanced.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,7 +10,7 @@ public class LogTraceConfig {
 
     @Bean
     public LogTrace logTrace() {
-        return new FieldLogTrace();
+        return new ThreadLocalLogTrace();
     }
 
 }
