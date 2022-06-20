97edd55a8348d63ced65fb2604d994d3d72a672b
Junhan0037 <wnsgks0037@naver.com>
2022-06-20 16:05:42 +0900

    feat: 전략 패턴 - 예제2

--- src/test/java/com/springadvanced/trace/strategy/ContextV1Test.java
+++ src/test/java/com/springadvanced/trace/strategy/ContextV1Test.java
@@ -1,6 +1,7 @@
 package com.springadvanced.trace.strategy;
 
 import com.springadvanced.trace.strategy.code.ContextV1;
+import com.springadvanced.trace.strategy.code.Strategy;
 import com.springadvanced.trace.strategy.code.StrategyLogic1;
 import com.springadvanced.trace.strategy.code.StrategyLogic2;
 import lombok.extern.slf4j.Slf4j;
@@ -46,6 +47,66 @@ public class ContextV1Test {
 
         StrategyLogic2 strategyLogic2 = new StrategyLogic2();
         ContextV1 context2 = new ContextV1(strategyLogic2);
+        context2.execute();
+    }
+
+    /**
+     * 전략 패턴 익명 내부 클래스1
+     */
+    @Test
+    void strategyV2() {
+        Strategy strategyLogic1 = new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+        };
+        log.info("strategyLogic1={}", strategyLogic1.getClass());
+        ContextV1 context1 = new ContextV1(strategyLogic1);
+        context1.execute();
+
+        Strategy strategyLogic2 = new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+        };
+        log.info("strategyLogic2={}", strategyLogic2.getClass());
+        ContextV1 context2 = new ContextV1(strategyLogic2);
+        context2.execute();
+    }
+
+    /**
+     * 전략 패턴 익명 내부 클래스2
+     */
+    @Test
+    void strategyV3() {
+        ContextV1 context1 = new ContextV1(new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+        });
+        context1.execute();
+
+        ContextV1 context2 = new ContextV1(new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직2 실행");
+            }
+        });
+        context2.execute();
+    }
+
+    /**
+     * 전략 패턴, 람다
+     */
+    @Test
+    void strategyV4() {
+        ContextV1 context1 = new ContextV1(() -> log.info("비즈니스 로직1 실행"));
+        context1.execute();
+
+        ContextV1 context2 = new ContextV1(() -> log.info("비즈니스 로직2 실행"));
         context2.execute();
     }
 
