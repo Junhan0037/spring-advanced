fe46937ae829e5da46b7972fb94bf82c5de7683c
Junhan0037 <wnsgks0037@naver.com>
2022-07-06 22:11:00 +0900

    feat: 구체 클래스 기반 프록시 - 적용

--- proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -1,6 +1,6 @@
 package hello.proxy;
 
-import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
+import hello.proxy.config.v1_proxy.ConcreteProxyConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
-@Import(InterfaceProxyConfig.class)
+@Import(ConcreteProxyConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app")
 public class ProxyApplication {
 
