9eeacece9b744f791ee9d5235b81aa34220a0c59
Junhan0037 <wnsgks0037@naver.com>
2022-08-05 13:27:27 +0900

    feat: 예제 코드2 - 직접 만든 포인트컷

--- proxy/src/test/java/hello/proxy/advisor/AdvisorTest.java
+++ proxy/src/test/java/hello/proxy/advisor/AdvisorTest.java
@@ -4,10 +4,15 @@ import hello.proxy.common.advice.TimeAdvice;
 import hello.proxy.common.service.ServiceImpl;
 import hello.proxy.common.service.ServiceInterface;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.aop.ClassFilter;
+import org.springframework.aop.MethodMatcher;
 import org.springframework.aop.Pointcut;
 import org.springframework.aop.framework.ProxyFactory;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
+
+import java.lang.reflect.Method;
 
 @Slf4j
 public class AdvisorTest {
@@ -28,6 +33,62 @@ public class AdvisorTest {
 
         proxy.save();
         proxy.find();
+    }
+
+    @Test
+    @DisplayName("직접 만든 포인트컷")
+    void advisorTest2() {
+        ServiceImpl target = new ServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(new MyPointcut(), new TimeAdvice());
+        proxyFactory.addAdvisor(advisor);
+        ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+
+        proxy.save();
+        proxy.find();
+    }
+
+    static class MyPointcut implements Pointcut {
+        @Override
+        public ClassFilter getClassFilter() {
+            return ClassFilter.TRUE;
+        }
+
+        @Override
+        public MethodMatcher getMethodMatcher() {
+            return new MyMethodMatcher();
+        }
+    }
+
+    static class MyMethodMatcher implements MethodMatcher {
+        private String matchName = "save";
+
+        /**
+         * 어드바이스를 적용할지 적용하지 않을지 판단할 수 있다.
+         */
+        @Override
+        public boolean matches(Method method, Class<?> targetClass) {
+            boolean result = method.getName().equals(matchName);
+            log.info("포인트컷 호출 method={}, targetClass={}", method.getName(), targetClass);
+            log.info("포인트컷 결과 result={}", result);
+            return result;
+        }
+
+        /**
+         * isRuntime() 이 값이 참이면 matches(...args) 메서드가 대신 호출된다.
+         * 동적으로 넘어오는 매개변수를 판단 로직으로 사용할 수 있다.
+         * isRuntime() 이 false 인 경우 클래스의 정적 정보만 사용하기 때문에, 스프링이 내부에서 캐싱을 통해 성능 향상이 가능하지만,
+         * true 인 경우 매개변수가 동적으로 변경된다고 가정 하기 때문에 캐싱을 하지 않는다.
+         */
+        @Override
+        public boolean isRuntime() {
+            return false;
+        }
+
+        @Override
+        public boolean matches(Method method, Class<?> targetClass, Object... args) {
+            throw new UnsupportedOperationException();
+        }
     }
 
 }
