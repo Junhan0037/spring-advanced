1ccd4a9786e9aa00ee735e39bdcd3727bfab066b
Junhan0037 <wnsgks0037@naver.com>
2022-08-17 09:37:55 +0900

    feat: @Aspect 프록시 - 적용

---
+++ proxy/src/main/java/hello/proxy/config/v6_aop/aspect/LogTraceAspect.java
@@ -0,0 +1,43 @@
+package hello.proxy.config.v6_aop.aspect;
+
+import hello.proxy.trace.TraceStatus;
+import hello.proxy.trace.logtrace.LogTrace;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+
+@Slf4j
+@Aspect // 에노테이션 기반 프록시를 적용
+public class LogTraceAspect {
+
+    private final LogTrace logTrace;
+
+    public LogTraceAspect(LogTrace logTrace) {
+        this.logTrace = logTrace;
+    }
+
+    @Around("execution(* hello.proxy.app..*(..))") // 포인트컷 표현식 (Aspect J 표현식)
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+        TraceStatus status = null;
+
+        log.info("target={}", joinPoint.getTarget()); // 실제 호출 대상
+        log.info("getArgs={}", joinPoint.getArgs()); // 전달인자
+        log.info("getSignature={}", joinPoint.getSignature()); // join point 시그니처
+
+        try {
+            String message = joinPoint.getSignature().toShortString();
+            status = logTrace.begin(message);
+
+            // 로직 호출
+            Object result = joinPoint.proceed();
+
+            logTrace.end(status);
+            return result;
+        } catch (Exception e) {
+            logTrace.exception(status, e);
+            throw e;
+        }
+    }
+
+}
