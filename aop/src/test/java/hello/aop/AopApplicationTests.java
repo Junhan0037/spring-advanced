2ba17f84bf27f7023df8ca84a2ba6dfc5e7eeccc
Junhan0037 <wnsgks0037@naver.com>
2022-08-19 11:31:08 +0900

    feat: 스프링 AOP 구현1 - 시작

---
+++ aop/src/test/java/hello/aop/AopApplicationTests.java
@@ -0,0 +1,39 @@
+package hello.aop;
+
+import hello.aop.order.OrderRepository;
+import hello.aop.order.OrderService;
+import hello.aop.order.aop.AspectV1;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@SpringBootTest
+@Import(AspectV1.class)
+@Slf4j
+class AopApplicationTests {
+
+    @Autowired OrderService orderService;
+    @Autowired OrderRepository orderRepository;
+
+    @Test
+    void appInfo() {
+        log.info("isAopProxy, orderService={}", AopUtils.isAopProxy(orderService));
+        log.info("isAopProxy, orderRepository={}", AopUtils.isAopProxy(orderRepository));
+    }
+
+    @Test
+    void success() {
+        orderService.orderItem("itemA");
+    }
+
+    @Test
+    void exception() {
+        assertThatThrownBy(() -> orderService.orderItem("ex")).isInstanceOf(IllegalStateException.class);
+    }
+
+}
