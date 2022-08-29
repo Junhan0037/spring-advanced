013fbea6eb0afed6dbf5d0ed0fd19bcdb5ee1dad
Junhan0037 <wnsgks0037@naver.com>
2022-08-30 08:24:41 +0900

    feat: 스프링 AOP - 포인트컷
    - within
    - args

---
+++ aop/src/test/java/hello/aop/pointcut/ArgsTest.java
@@ -0,0 +1,63 @@
+package hello.aop.pointcut;
+
+import hello.aop.member.MemberServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
+
+import java.lang.reflect.Method;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * 인자가 주어진 타입의 인스턴스인 조인 포인트로 매칭
+ * 기본 문법은 execution 의 args 부분과 같다.
+ * args 는 부모 타입을 허용한다. 실제 넘어온 파라미터 객체 인스턴스를 보고 판단한다.
+ * 단독으로 사용되기 보다는 파라미터 바인딩에서 주로 사용된다.
+ */
+@Slf4j
+public class ArgsTest {
+
+    Method helloMethod;
+
+    @BeforeEach
+    public void init() throws NoSuchMethodException {
+        helloMethod = MemberServiceImpl.class.getMethod("hello", String.class);
+    }
+
+    private AspectJExpressionPointcut pointcut(String expression) {
+        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+        pointcut.setExpression(expression);
+        return pointcut;
+    }
+
+    @Test
+    void args() {
+        // hello(String)과 매칭
+        assertThat(pointcut("args(String)").matches(helloMethod, MemberServiceImpl.class)).isTrue();
+        assertThat(pointcut("args(Object)").matches(helloMethod, MemberServiceImpl.class)).isTrue();
+        assertThat(pointcut("args()").matches(helloMethod, MemberServiceImpl.class)).isFalse();
+        assertThat(pointcut("args(..)").matches(helloMethod, MemberServiceImpl.class)).isTrue();
+        assertThat(pointcut("args(*)").matches(helloMethod, MemberServiceImpl.class)).isTrue();
+        assertThat(pointcut("args(String,..)").matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+    /**
+     * execution(* *(java.io.Serializable)): 메서드의 시그니처로 판단 (정적)
+     * args(java.io.Serializable): 런타임에 전달된 인수로 판단 (동적)
+     */
+    @Test
+    void argsVsExpression() {
+        //Args
+        assertThat(pointcut("args(String)").matches(helloMethod, MemberServiceImpl.class)).isTrue();
+        assertThat(pointcut("args(java.io.Serializable)").matches(helloMethod, MemberServiceImpl.class)).isTrue();
+        assertThat(pointcut("args(Object)").matches(helloMethod, MemberServiceImpl.class)).isTrue();
+
+        //Execution
+        assertThat(pointcut("execution(* *(String))").matches(helloMethod, MemberServiceImpl.class)).isTrue();
+        assertThat(pointcut("execution(* *(java.io.Serializable))").matches(helloMethod, MemberServiceImpl.class)).isFalse(); // 매칭 실패
+        assertThat(pointcut("execution(* *(Object))").matches(helloMethod, MemberServiceImpl.class)).isFalse(); // 매칭 실패
+    }
+
+}
---
+++ aop/src/test/java/hello/aop/pointcut/WithinTest.java
@@ -0,0 +1,58 @@
+package hello.aop.pointcut;
+
+import hello.aop.member.MemberServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
+
+import java.lang.reflect.Method;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * within 지시자는 특정 타입 내의 조인 포인트에 대한 매칭을 제한한다.
+ * 해당 타입이 매칭되면 그 안의 메서드(조인 포인트)들이 자동으로 매칭된다.
+ */
+@Slf4j
+public class WithinTest {
+
+    AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+    Method helloMethod;
+
+    @BeforeEach
+    public void init() throws NoSuchMethodException {
+        helloMethod = MemberServiceImpl.class.getMethod("hello", String.class);
+    }
+
+    @Test
+    void withinExact() {
+        pointcut.setExpression("within(hello.aop.member.MemberServiceImpl)");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+    @Test
+    void withinStar() {
+        pointcut.setExpression("within(hello.aop.member.*Service*)");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+    @Test
+    void withinSubPackage() {
+        pointcut.setExpression("within(hello.aop..*)");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+    @Test
+    @DisplayName("타켓의 타입에만 직접 적용, 인터페이스를 선정하면 안된다.")
+    void withinSuperTypeFalse() {
+        pointcut.setExpression("within(hello.aop.member.MemberService)");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isFalse();
+    }
+    @Test
+    @DisplayName("execution은 타입 기반, 인터페이스를 선정 가능.")
+    void executionSuperTypeTrue() {
+        pointcut.setExpression("execution(* hello.aop.member.MemberService.*(..))");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+}
